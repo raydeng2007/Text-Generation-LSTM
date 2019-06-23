44568ffefe2f226d3a0932076c850d1feb81cb1c
Raymond <r6deng@uwaterloo.ca>
2019-06-23 14:20:01 -0400

    updated with asynchornous update using socket.io, drastically increase speed

--- script.py
+++ script.py
@@ -9,58 +9,21 @@ import json
 from keras.preprocessing.text import text_to_word_sequence
 import numpy as np
 import flask
-from flask import Flask, render_template,url_for,request
+from flask import Flask, render_template,url_for,request,copy_current_request_context
+import time
+from flask_socketio import SocketIO, emit
+
 
 #creating instance of the class
+
 app=Flask(__name__)
-
-#to tell flask what url should trigger the function index()
-@app.route('/')
-@app.route('/index')
-def index():
-    return flask.render_template('index.html')
-
-@app.route('/predict',methods=['POST'])
-def predict():
-    char_to_int = {' ': 0,
-                   '0': 1,
-                   '1': 2,
-                   '2': 3,
-                   '3': 4,
-                   '4': 5,
-                   '5': 6,
-                   '6': 7,
-                   '7': 8,
-                   '8': 9,
-                   '9': 10,
-                   'a': 11,
-                   'b': 12,
-                   'c': 13,
-                   'd': 14,
-                   'e': 15,
-                   'f': 16,
-                   'g': 17,
-                   'h': 18,
-                   'i': 19,
-                   'j': 20,
-                   'k': 21,
-                   'l': 22,
-                   'm': 23,
-                   'n': 24,
-                   'o': 25,
-                   'p': 26,
-                   'q': 27,
-                   'r': 28,
-                   's': 29,
-                   't': 30,
-                   'u': 31,
-                   'v': 32,
-                   'w': 33,
-                   'x': 34,
-                   'y': 35,
-                   'z': 36}
-
-    int_to_char = {0: ' ',
+socketio = SocketIO(app)
+global m
+m = load_model('model')
+m._make_predict_function()
+global n_vocab
+global int_to_char
+int_to_char = {0: ' ',
                    1: '0',
                    2: '1',
                    3: '2',
@@ -97,13 +60,28 @@ def predict():
                    34: 'x',
                    35: 'y',
                    36: 'z'}
-    n_vocab = 37
+n_vocab = 37
 
-    m = load_model('model')
+is_text_box_empty=True
+#to tell flask what url should trigger the function index()
+@app.route('/',methods=['GET','POST'])
+@app.route('/index',methods=['GET','POST'])
+def index():
+    return flask.render_template('index.html',async_mode=socketio.async_mode)
 
 
+@socketio.on('input')
+def test_message(input_words):
+    global is_text_box_empty
+    input_words = input_words['data']
+    if is_text_box_empty:
 
-    input_words = request.form['comment']
+        is_text_box_empty = False
+    else:
+        socketio.emit('clear')
+        #global is_text_box_empty
+        is_text_box_empty = True
+
     if len(input_words) < 100:
         # this needs to be at least 100
         print('yo there something wrong here lil bro')
@@ -119,7 +97,6 @@ def predict():
             return np.argmax(probas)
 
         pattern = []
-        input = input_words
         input_words = input_words[-100:]
         for char in input_words:
             try:
@@ -127,133 +104,19 @@ def predict():
             except:
                 pattern.append(0)
         output = ''
-        for i in range(1000):
+        for i in range(800):
             x = np.reshape(pattern, (1, len(pattern), 1))
             x = x / float(n_vocab)
             prediction = m.predict(x, verbose=0)
-            m._make_predict_function()
             index = sample(prediction)
-            # index =  np.argmax(prediction)
             result = int_to_char[index]
-            seq_in = [int_to_char[value] for value in pattern]
-            output = output+result
+            output = output + result
             pattern.append(index)
             pattern = pattern[1:len(pattern)]
-
-
-    return render_template('result.html', prediction=output, input=input)
-
-
-class model():
-    def __init__(self):
-        self.model = load_model('model')
-        self.char_to_int = {' ': 0,
-                             '0': 1,
-                             '1': 2,
-                             '2': 3,
-                             '3': 4,
-                             '4': 5,
-                             '5': 6,
-                             '6': 7,
-                             '7': 8,
-                             '8': 9,
-                             '9': 10,
-                             'a': 11,
-                             'b': 12,
-                             'c': 13,
-                             'd': 14,
-                             'e': 15,
-                             'f': 16,
-                             'g': 17,
-                             'h': 18,
-                             'i': 19,
-                             'j': 20,
-                             'k': 21,
-                             'l': 22,
-                             'm': 23,
-                             'n': 24,
-                             'o': 25,
-                             'p': 26,
-                             'q': 27,
-                             'r': 28,
-                             's': 29,
-                             't': 30,
-                             'u': 31,
-                             'v': 32,
-                             'w': 33,
-                             'x': 34,
-                             'y': 35,
-                             'z': 36}
-
-        self.int_to_char = {0: ' ',
-                             1: '0',
-                             2: '1',
-                             3: '2',
-                             4: '3',
-                             5: '4',
-                             6: '5',
-                             7: '6',
-                             8: '7',
-                             9: '8',
-                             10: '9',
-                             11: 'a',
-                             12: 'b',
-                             13: 'c',
-                             14: 'd',
-                             15: 'e',
-                             16: 'f',
-                             17: 'g',
-                             18: 'h',
-                             19: 'i',
-                             20: 'j',
-                             21: 'k',
-                             22: 'l',
-                             23: 'm',
-                             24: 'n',
-                             25: 'o',
-                             26: 'p',
-                             27: 'q',
-                             28: 'r',
-                             29: 's',
-                             30: 't',
-                             31: 'u',
-                             32: 'v',
-                             33: 'w',
-                             34: 'x',
-                             35: 'y',
-                             36: 'z'}
-
-        self.n_vocab = 37
-        #self.model._make_predict_function()
-
-    def sample(self,preds, temperature=0.8):
-        # helper function to sample an index from a probability array
-        preds = np.asarray(preds[0]).astype('float64')
-        preds = np.log(preds) / temperature
-        exp_preds = np.exp(preds)
-        preds = exp_preds / np.sum(exp_preds)
-        probas = np.random.multinomial(1, preds, 1)
-        return np.argmax(probas)
-
-
-
-
-
-
-
-
-
-
-
-def result():
-    m = model()
-    if request.method == 'POST':
-        word_to_predict = request.form.to_string()
-        result = m.predict(word_to_predict)
-
+            emit('my_response', {'data': result})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    socketio.run(app)
 
 
 
